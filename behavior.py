952776fb9b12b2dff12ab89d6abd45e3ef58159b
Jacob Lewallen <jlewallen@gmail.com>
2020-10-13 18:35:33 -0700

    Stub for behavior stuff.

---
+++ behavior.py
@@ -0,0 +1,39 @@
+import lupa
+import game
+import props
+
+
+class Behavior(props.PropertyMap):
+    def __init__(self, world, person):
+        self.world = world
+        self.person = person
+
+    def execute(self, hook):
+        lua = lupa.LuaRuntime(unpack_returned_tuples=True)
+        g = lua.globals()
+        g.world = self.world
+        g.player = self.jacob
+        g.item = None
+
+
+class Hook:
+    def __init__(self, **kwargs):
+        self.item = kwargs["item"] if "item" in kwargs else None
+
+    def prepare(self):
+        pass
+
+    def prepare(self):
+        pass
+
+
+class CreatedHook(Hook):
+    pass
+
+
+class HeldHook(Hook):
+    pass
+
+
+class DropHook(Hook):
+    pass
