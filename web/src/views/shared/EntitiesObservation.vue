dde2f0956f09f7400cb777bbe6af4a19f8a21c7b
Jacob Lewallen <jlewallen@gmail.com>
2020-10-15 17:31:20 -0700

    Render entity observations.

---
+++ web/src/views/shared/EntitiesObservation.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="response area-observation card">
+        <div class="card-body">
+            <div class="entities">
+                <div v-for="entity in response.reply.entities" v-bind:key="entity.key">
+                    <TinyEntityPanel :entity="entity" @selected="(e) => onSelected(e)" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Entity, ReplResponse } from "@/store";
+import TinyEntityPanel from "../entity/TinyEntityPanel.vue";
+
+export default defineComponent({
+    name: "EntitiesObservation",
+    components: {
+        TinyEntityPanel,
+    },
+    props: {
+        response: {
+            type: Object as () => ReplResponse,
+            required: true,
+        },
+    },
+    data(): {} {
+        return {};
+    },
+    computed: {},
+    methods: {
+        onSelected(entity: Entity): void {
+            this.$emit("selected", entity);
+        },
+    },
+});
+</script>
+
+<style scoped>
+.entities {
+    display: flex;
+}
+</style>
--- web/src/views/shared/replies.ts
+++ web/src/views/shared/replies.ts
@@ -1,7 +1,8 @@
 import AreaObservation from "./AreaObservation.vue";
 import DetailedObservation from "./DetailedObservation.vue";
 import PersonalObservation from "./PersonalObservation.vue";
+import EntitiesObservation from "./EntitiesObservation.vue";
 import Success from "./Success.vue";
 import Failure from "./Failure.vue";
 
-export default { AreaObservation, DetailedObservation, PersonalObservation, Success, Failure };
+export default { AreaObservation, EntitiesObservation, DetailedObservation, PersonalObservation, Success, Failure };
