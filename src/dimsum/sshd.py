a4766a8c84c3143016cc462d3bd360b39b23e08f
Jacob Lewallen <jlewallen@gmail.com>
2020-12-30 19:55:11 -0800

    Fix prompt.

--- src/dimsum/sshd.py
+++ src/dimsum/sshd.py
@@ -1,7 +1,8 @@
-from typing import Any, List
+from typing import Any, List, Union
 
 import asyncio
 import asyncssh
+import time
 import crypt
 import sys
 import logging
@@ -13,6 +14,9 @@ import evaluator
 import messages
 import rich
 import rich.console
+import rich.live
+import rich.table
+
 
 log = logging.getLogger("dimsum.sshd")
 
@@ -38,6 +42,7 @@ class ShellSession:
 
     def __init__(self, state, process):
         super().__init__()
+        self.l = grammar.create_parser()
         self.state = state
         self.process = process
         self.name = self.process.get_extra_info("username")
@@ -58,57 +63,65 @@ class ShellSession:
         await self.state.save()
         return player
 
+    def parse_as(self, evaluator, full):
+        tree = self.l.parse(full.strip())
+        log.info(str(tree))
+        return evaluator.transform(tree)
+
+    async def iteration(self):
+        command = await self.read_command()
+
+        if command is None:
+            return False
+
+        if command == "":
+            return True
+
+        world = self.state.world
+        player = await self.get_player()
+        action = self.parse_as(evaluator.create(world, player), command)
+        reply = await world.perform(action, player)
+        await self.state.save()
+
+        visitor = messages.ReplyVisitor()
+        visual = reply.accept(visitor)
+        log.info(
+            "%s" % (visual),
+        )
+
+        self.write("\n", end="")
+
+        if "title" in visual:
+            self.write(visual["title"], end="")
+            self.write("\n", end="")
+
+        if "text" in visual:
+            self.write(visual["text"], end="")
+            self.write("\n", end="")
+
+        if "description" in visual:
+            self.write("\n", end="")
+            self.write(visual["description"], end="")
+
+        self.write("\n", end="")
+
+        return True
+
     async def repl(self):
-        l = grammar.create_parser()
-
-        def parse_as(evaluator, full):
-            tree = l.parse(full.strip())
-            log.info(str(tree))
-            return evaluator.transform(tree)
-
         while True:
-            command = await self.read_command()
-
-            if command is None:
-                break
-
-            if command == "":
-                continue
-
-            world = self.state.world
-            player = await self.get_player()
-            action = parse_as(evaluator.create(world, player), command)
-            reply = await world.perform(action, player)
-            await self.state.save()
-
-            visitor = messages.ReplyVisitor()
-            visual = reply.accept(visitor)
-            log.info(
-                "%s" % (visual),
-            )
-
-            self.write("\n", end="")
-
-            if "title" in visual:
-                self.write(visual["title"], end="")
+            try:
+                safe = await self.iteration()
+                if not safe:
+                    break
+            except asyncssh.TerminalSizeChanged as exc:
+                log.info("%s: terminal size changed %s" % (self.name, str(exc)))
+                self.recreate_console()
                 self.write("\n", end="")
 
-            if "text" in visual:
-                self.write(visual["text"], end="")
-                self.write("\n", end="")
-
-            if "description" in visual:
-                self.write("\n", end="")
-                self.write(visual["description"], end="")
-
-            self.write("\n", end="")
-
-    async def run(self):
+    def recreate_console(self):
         term_type = self.process.get_terminal_type()
-        width, height, pixwidth, pixheight = self.process.get_terminal_size()
-
-        name = self.process.get_extra_info("username")
-
+        width, height, width_pixels, height_pixels = self.process.get_terminal_size()
+        print("%s: recreating (%d x %d)" % (self.name, width, height))
         self.console = rich.console.Console(
             file=WrapStandardOut(self.process.stdout),
             force_terminal=True,
@@ -116,43 +129,63 @@ class ShellSession:
             height=height,
         )
 
-        if self.process.env:
-            for key, value in self.process.env.items():
-                self.process.stdout.write("%s=%s\n" % (key, value))
+    async def run(self):
+        self.name = self.process.get_extra_info("username")
 
-        log.info("%s: connected: %s (%d x %d)" % (name, term_type, width, height))
+        self.recreate_console()
+
+        log.info("%s: connected" % (self.name))
 
         if not self.state.world:
             self.print("\ninitializing...\n")
             self.process.exit(0)
             return
 
+        if False:
+            table = rich.table.Table()
+            table.add_column("Row ID")
+            table.add_column("Description")
+            table.add_column("Level")
+
+            with rich.live.Live(table, refresh_per_second=4, console=self.console):
+                for row in range(12):
+                    time.sleep(0.4)
+                    table.add_row(f"{row}", f"description {row}", "[red]ERROR")
+
         player = await self.get_player()
 
         self.others.append(self)
 
-        self.print("Welcome to the party, %s!" % (name,))
-        self.print("%d other users are connected." % len(self.others))
-        self.print("\n", end="")
-
-        self.write_everyone_else("*** %s has joined" % name)
-
         try:
-            await self.repl()
-        except asyncssh.BreakReceived:
-            log.info("%s: brk" % (name,))
+            self.print("\n", end="")
 
-        log.info("%s: disconnected" % (name,))
+            if False:
+                if self.process.env:
+                    for key, value in self.process.env.items():
+                        self.process.stdout.write("%s=%s\n" % (key, value))
 
-        self.write_everyone_else("*** %s has left" % name)
+            self.print("Welcome to the party, %s!" % (self.name,))
+            self.print("%d other users are connected." % len(self.others))
+            self.print("\n", end="")
 
-        self.others.remove(self)
-        self.process.exit(0)
+            self.write_everyone_else("*** %s has joined" % self.name)
+
+            try:
+                await self.repl()
+            except asyncssh.BreakReceived:
+                log.info("%s: brk" % (self.name,))
+
+            log.info("%s: disconnected" % (self.name,))
+
+            self.write_everyone_else("*** %s has left" % self.name)
+        finally:
+            self.others.remove(self)
+            self.process.exit(0)
 
     def prompt(self) -> str:
-        return " > "
+        return ">> "
 
-    async def read_command(self) -> str:
+    async def read_command(self) -> Union[str, None]:
         line = await self.console.input(prompt=self.prompt(), stream=self.process.stdin)
         if isinstance(line, str):
             if len(line) == 0:
@@ -163,10 +196,8 @@ class ShellSession:
     def write_everyone_else(self, msg: str):
         for other in self.others:
             if other != self:
-                other.control("\r\033[2K")
-                other.write(msg)
-                other.control("\033[2K")
-                other.write("\n", end="")
+                other.control("\033[1G\033[2K")
+                other.write(msg + "\n")
                 other.write(self.prompt(), end="")
 
     def control(self, msg: str, **kwargs):
