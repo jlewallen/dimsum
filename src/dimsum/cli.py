7e01a06dd9f93e9fbdeaa28dc57b6e5d101e8c1f
Jacob Lewallen <jlewallen@gmail.com>
2021-06-16 17:18:48 -0700

    feat: Quick and dirty repl mode.

--- src/dimsum/cli.py
+++ src/dimsum/cli.py
@@ -10,10 +10,111 @@ import os
 
 import luaproxy
 import handlers
+import properties
 import world
 import messages
 import persistence
 import entity
+import animals
+import actions
+import library
+
+import grammar
+import evaluator
+
+log = logging.getLogger("dimsum-repl")
+
+
+class Repl:
+    def __init__(self, fn: str, name: str):
+        super().__init__()
+        self.l = grammar.create_parser()
+        self.fn = fn
+        self.name = name
+        self.world = None
+        self.bus = messages.TextBus(handlers=[handlers.WhateverHandlers])
+        self.db = persistence.SqliteDatabase()
+
+    async def get_player(self):
+        if self.world is None:
+            self.world = world.World(self.bus, luaproxy.context_factory)
+            await self.db.open(self.fn)
+            await self.db.load(self.world)
+
+        if self.world.empty():
+            log.info("creating example world")
+            generics, area = library.create_example_world(self.world)
+            self.world.add_entities(generics.all)
+            self.world.add_area(area)
+            await self.db.save(self.world)
+
+        if self.world.contains(self.name):
+            player = self.world.find_by_key(self.name)
+            return player
+
+        player = animals.Player(
+            key=self.name,
+            creator=self.world,
+            props=properties.Common(self.name, desc="A repl user"),
+        )
+        await self.world.perform(actions.Join(), player)
+        await self.save()
+        return player
+
+    def parse_as(self, evaluator, full):
+        tree = self.l.parse(full.strip())
+        log.info(str(tree))
+        return evaluator.transform(tree)
+
+    async def save(self):
+        db = persistence.SqliteDatabase()
+        await db.open(self.fn)
+        await db.save(self.world)
+
+    async def read_command(self):
+        return sys.stdin.readline().strip()
+
+    async def iteration(self):
+        player = await self.get_player()
+
+        command = await self.read_command()
+
+        if command is None:
+            return False
+
+        if command == "":
+            return True
+
+        action = self.parse_as(evaluator.create(self.world, player), command)
+        reply = await self.world.perform(action, player)
+        await self.save()
+
+        visitor = messages.ReplyVisitor()
+        visual = reply.accept(visitor)
+        log.info(
+            "%s" % (visual),
+        )
+
+        self.write("\n", end="")
+
+        if "title" in visual:
+            self.write(visual["title"], end="")
+            self.write("\n", end="")
+
+        if "text" in visual:
+            self.write(visual["text"], end="")
+            self.write("\n", end="")
+
+        if "description" in visual:
+            self.write("\n", end="")
+            self.write(visual["description"], end="")
+
+        self.write("\n", end="")
+
+        return True
+
+    def write(self, s: str, end=None):
+        sys.stdout.write(s)
 
 
 def get_color(e: entity.Entity) -> str:
@@ -46,19 +147,23 @@ async def main():
     args = parser.parse_args()
 
     for path in args.databases:
-        bus = messages.TextBus(handlers=[handlers.WhateverHandlers])
-        the_world = world.World(bus, luaproxy.context_factory)
-        db = persistence.SqliteDatabase()
-        await db.open(path)
-        await db.load(the_world)
-
         if args.graph:
+            bus = messages.TextBus(handlers=[handlers.WhateverHandlers])
+            the_world = world.World(bus, luaproxy.context_factory)
+            db = persistence.SqliteDatabase()
+            await db.open(path)
+            await db.load(the_world)
+
             name = os.path.splitext(path)[0]
             await db.write("{0}.json".format(name))
             await graph("{0}.dot".format(name), the_world)
 
         if args.repl:
-            pass
+            repl = Repl(path, "jlewallen")
+            while True:
+                safe = await repl.iteration()
+                if not safe:
+                    break
 
 
 if __name__ == "__main__":
