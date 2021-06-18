c04a46d87a01a753d8d56223a126bc65b290d5bc
Jacob Lewallen <jlewallen@gmail.com>
2021-06-17 17:26:25 -0700

    refact: Specialization.

--- src/dimsum/test_chimeras.py
+++ src/dimsum/test_chimeras.py
@@ -20,11 +20,9 @@ log = logging.getLogger("dimsum")
 
 
 class SimpleCore(entity.Spawned):
-    def __init__(self, name: str = None, frozen=None, destroyed=None, **kwargs):
+    def __init__(self, name: str = None, **kwargs):
         super().__init__(**kwargs)
         self.name = name
-        self.frozen = frozen
-        self.destroyed = destroyed
 
 
 class SimpleHolding(entity.Spawned):
@@ -51,6 +49,56 @@ async def test_chimeric_entities_serialize(caplog):
         with jacob.make(SimpleCore) as core:
             pass
         holding.add_item(toy)
+
+    db = persistence.SqliteDatabase()
+    await db.open("test.sqlite3")
+    await db.purge()
+    await db.save(universe)
+
+    empty = world.World(bus, context_factory=universe.context_factory)
+    await db.load(empty)
+
+    assert await db.number_of_entities() == 3
+
+
+def make_person(props: properties.Common = None, **kwargs):
+    person = entity.Entity(props=props, **kwargs)
+
+    assert props
+
+    with person.make(SimpleCore) as core:
+        core.name = props.name
+
+    with person.make(SimpleHolding) as holding:
+        pass
+
+    return person
+
+
+def make_thing(props: properties.Common = None, **kwargs):
+    thing = entity.Entity(props=props, **kwargs)
+
+    assert props
+
+    with thing.make(SimpleCore) as core:
+        core.name = props.name
+
+    with thing.make(SimpleHolding) as holding:
+        pass
+
+    return thing
+
+
+@pytest.mark.asyncio
+async def test_specialization_classes(caplog):
+    bus = messages.TextBus(handlers=[handlers.WhateverHandlers])
+    universe = world.World(bus, luaproxy.context_factory)
+
+    person = make_person(creator=universe, props=properties.Common(name="Jacob"))
+    universe.register(person)
+
+    toy = make_thing(creator=universe, props=properties.Common(name="Toy"))
+    universe.register(toy)
 
     db = persistence.SqliteDatabase()
     await db.open("test.sqlite3")
