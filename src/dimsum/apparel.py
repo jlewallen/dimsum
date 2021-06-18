f5ca595750b355977ed8214744a05e634bd38b02
Jacob Lewallen <jlewallen@gmail.com>
2021-06-18 09:33:58 -0700

    refact: Remove WearableMixin, unused.

--- src/dimsum/apparel.py
+++ src/dimsum/apparel.py
@@ -5,29 +5,18 @@ import abc
 import entity
 
 
-class WearableMixin:
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    @abc.abstractmethod
-    def touch(self):
-        pass
-
-
 class ApparelMixin(entity.Scope):
     def __init__(self, wearing=None, **kwargs):
         super().__init__(**kwargs)
-        self.wearing: List[WearableMixin] = wearing if wearing else []
+        self.wearing: List[entity.Entity] = wearing if wearing else []
 
-    def is_wearing(self, item: WearableMixin) -> bool:
+    def is_wearing(self, item: entity.Entity) -> bool:
         return item in self.wearing
 
-    def wear(self, item: WearableMixin) -> bool:
+    def wear(self, item: entity.Entity) -> bool:
         self.wearing.append(item)
-        item.touch()
         return True
 
-    def unwear(self, item: WearableMixin, **kwargs) -> bool:
+    def unwear(self, item: entity.Entity, **kwargs) -> bool:
         self.wearing.remove(item)
-        item.touch()
         return True
