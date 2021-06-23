bb545a985b32f518b947571345f899bd143b5ba6
Jacob Lewallen <jlewallen@gmail.com>
2021-06-23 07:36:08 -0700

    chore: Unused.

--- src/dimsum/test_storage_http.py
+++ src/dimsum/test_storage_http.py
@@ -14,23 +14,6 @@ import dimsum
 log = logging.getLogger("dimsum")
 
 
-class Server(uvicorn.Server):
-    def install_signal_handlers(self):
-        pass
-
-    @contextlib.contextmanager
-    def run_in_thread(self):
-        thread = threading.Thread(target=self.run)
-        thread.start()
-        try:
-            while not self.started:
-                time.sleep(1e-3)
-            yield
-        finally:
-            self.should_exit = True
-            thread.join()
-
-
 @pytest.fixture(scope="session")
 def server():
     log.info("started server")
