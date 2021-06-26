137ebec3b47efffa054b91e99f72008032ce40af
Jacob Lewallen <jlewallen@gmail.com>
2021-06-26 09:08:23 -0700

    chore: Unused.

--- src/dimsum/utils.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-import concurrent.futures
-
-pool = concurrent.futures.ThreadPoolExecutor()
-
-
-def run_async_on_other_thread(coroutine):
-    return pool.submit(asyncio.run, coroutine).result()
