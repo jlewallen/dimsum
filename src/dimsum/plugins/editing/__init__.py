17eed1e9fbe7fbbb886a04ddb2d90ecac39de03b
Jacob Lewallen <jlewallen@gmail.com>
2021-07-02 16:46:11 -0700

    feat: Clear screen via cls :)

--- src/dimsum/plugins/editing/__init__.py
+++ src/dimsum/plugins/editing/__init__.py
@@ -9,6 +9,7 @@ from model.world import *
 from model.events import *
 from model.things import *
 from model.reply import *
+from model.game import *
 
 from plugins.actions import *
 from context import *
@@ -17,7 +18,7 @@ log = logging.getLogger("dimsum")
 
 
 @dataclasses.dataclass
-class EditableEntity(StandardEvent):
+class EditingEntity(StandardEvent):
     entity: Entity
 
 
@@ -32,8 +33,20 @@ class EditEntity(PersonAction):
     ):
         item = await world.apply_item_finder(person, self.item)
         if item:
-            return EditableEntity(entity=item, living=person, area=area, heard=[])
+            return EditingEntity(entity=item, living=person, area=area, heard=[])
         return Failure("where's that?")
+
+
+@dataclasses.dataclass
+class ScreenCleared(Reply):
+    pass
+
+
+class ClearScreen(PersonAction):
+    async def perform(
+        self, world: World, area: Entity, person: Entity, ctx: Ctx, **kwargs
+    ):
+        return ScreenCleared()
 
 
 @grammars.grammar()
@@ -45,12 +58,16 @@ class Grammar(grammars.Grammar):
     @property
     def lark(self) -> str:
         return """
-        start:             edit_entity
+        start:             edit_entity | clear_screen
 
         edit_entity:       ("edit" | "ed") noun
+        clear_screen:      "cls"
 """
 
 
 class Evaluator(BaseEvaluator):
     def edit_entity(self, args):
         return EditEntity(args[0])
+
+    def clear_screen(self, args):
+        return ClearScreen()
