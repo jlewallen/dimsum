db4ee1657fd0b3b3f3ded9dacb1e3a972f31b82f
Jacob Lewallen <jlewallen@gmail.com>
2021-07-11 15:15:42 -0700

    feat: Proof of concept of wild json diff acls idea.

--- src/dimsum/model/permissions.py
+++ src/dimsum/model/permissions.py
@@ -1,7 +1,9 @@
 import dataclasses
 import logging
 import enum
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Any
+
+log = logging.getLogger("dimsum")
 
 EverybodyIdentity = "*"
 
@@ -33,3 +35,65 @@ class Acls:
     def add(self, p: Permission, identity: str):
         self.rules.append(Acl(p, [identity]))
         return self
+
+
+@dataclasses.dataclass
+class SecurityCheck:
+    acls: List[Acl] = dataclasses.field(default_factory=list)
+
+
+AclsKey = "acls"
+
+
+def _walk_diff(original: Dict[str, Any], diff: Dict[str, Any]):
+    log.info("walking diff: %s", diff)
+
+    def prepare_acl(d):
+        del d["py/object"]
+        return d
+
+    def walk(frame, value):
+        # Maybe we only consider the Acls nearest to the editing
+        # position, or should we consider all of them. It's easier
+        # to just do all of them for now.
+        if isinstance(value, str):
+            return []
+        if isinstance(value, list):
+            return []
+        if isinstance(value, dict):
+            acls = []
+            if AclsKey in value:
+                acls.append(prepare_acl(value[AclsKey]))
+            return {
+                key: walk_into(frame, key, value) + acls for key, value in value.items()
+            }
+        return value
+
+    def walk_into(frame, key, value):
+        acls = []
+        if AclsKey in frame:
+            acls.append(prepare_acl(frame[AclsKey]))
+        walked = walk(frame[key], value)
+        if isinstance(walked, list):
+            return acls + walked
+        if isinstance(walked, dict):
+            return acls + flatten([v for _, v in walked.items()])
+        return acls
+
+    walked = walk(original, diff)
+    acl_maps = flatten([v for _, v in walked.items()])
+    return [Acls(**v) for v in acl_maps]
+
+
+def generate_security_check_from_json_diff(
+    original: Dict[str, Any], diff: Dict[str, Any]
+) -> SecurityCheck:
+    """
+    Walks the given jsondiff and pulls Acl objects from the original
+    json along the way.
+    """
+    return SecurityCheck(_walk_diff(original, diff))
+
+
+def flatten(l):
+    return [item for sl in l for item in sl]
--- src/dimsum/test_permissions.py
+++ src/dimsum/test_permissions.py
@@ -1,11 +1,122 @@
 import logging
+import dataclasses
+import jsondiff
+import json
+import functools
 import pytest
+from typing import Any, Dict, List
 
+import serializing
 from model import *
 from model.permissions import *
 import test
 
 log = logging.getLogger("dimsum")
+
+
+@dataclasses.dataclass
+class ExampleObject:
+    acls: Acls = dataclasses.field(
+        default_factory=functools.partial(Acls, "example-object")
+    )
+    value: str = "Original"
+
+
+@dataclasses.dataclass
+class InnerObject:
+    example: ExampleObject = dataclasses.field(default_factory=ExampleObject)
+    value: str = "Original"
+
+
+@dataclasses.dataclass
+class ExampleTree:
+    acls: Acls = dataclasses.field(
+        default_factory=functools.partial(Acls, "example-tree")
+    )
+    left: InnerObject = dataclasses.field(default_factory=InnerObject)
+    collection: List[InnerObject] = dataclasses.field(default_factory=list)
+    value: str = "Original"
+
+
+def acl_names(acls) -> List[str]:
+    return [a.name for a in acls]
+
+
+@dataclasses.dataclass
+class SecurityCheckBuilder:
+    pass
+
+
+@pytest.mark.asyncio
+async def test_permissions_wild_json_idea_1():
+    tree = ExampleTree()
+    before = serializing.serialize(tree, indent=True)
+    tree.value = "Modified"
+    after = serializing.serialize(tree, indent=True)
+    assert before and after
+    d = jsondiff.diff(json.loads(before), json.loads(after), marshal=True)
+    check = generate_security_check_from_json_diff(json.loads(before), d)
+    assert acl_names(check.acls) == ["example-tree"]
+
+
+@pytest.mark.asyncio
+async def test_permissions_wild_json_idea_2():
+    tree = ExampleTree()
+    before = serializing.serialize(tree, indent=True)
+    tree.left.value = "Modified"
+    after = serializing.serialize(tree, indent=True)
+    assert before and after
+    d = jsondiff.diff(json.loads(before), json.loads(after), marshal=True)
+    check = generate_security_check_from_json_diff(json.loads(before), d)
+    assert acl_names(check.acls) == ["example-tree"]
+
+
+@pytest.mark.asyncio
+async def test_permissions_wild_json_idea_3():
+    tree = ExampleTree()
+    before = serializing.serialize(tree, indent=True)
+    tree.left.example.value = "Modified"
+    after = serializing.serialize(tree, indent=True)
+    assert before and after
+    d = jsondiff.diff(json.loads(before), json.loads(after), marshal=True)
+    log.info("%s", before)
+    log.info("%s", after)
+    log.info("%s", d)
+    check = generate_security_check_from_json_diff(json.loads(before), d)
+    assert acl_names(check.acls) == ["example-tree", "example-object"]
+
+
+@pytest.mark.asyncio
+async def test_permissions_wild_json_idea_4():
+    tree = ExampleTree()
+    before = serializing.serialize(tree, indent=True)
+    tree.collection.append(InnerObject())
+    after = serializing.serialize(tree, indent=True)
+    assert before and after
+    d = jsondiff.diff(json.loads(before), json.loads(after), marshal=True)
+    log.info("%s", before)
+    log.info("%s", after)
+    log.info("%s", d)
+    check = generate_security_check_from_json_diff(json.loads(before), d)
+    assert acl_names(check.acls) == ["example-tree"]
+
+
+@pytest.mark.asyncio
+async def test_permissions_wild_json_idea_5():
+    tree = ExampleTree()
+    tree.collection.append(InnerObject())
+    tree.collection.append(InnerObject())
+    tree.collection.append(InnerObject())
+    before = serializing.serialize(tree, indent=True)
+    tree.collection[1].example.value = "Modified"
+    after = serializing.serialize(tree, indent=True)
+    assert before and after
+    d = jsondiff.diff(json.loads(before), json.loads(after), marshal=True)
+    log.info("%s", before)
+    log.info("%s", after)
+    log.info("%s", d)
+    check = generate_security_check_from_json_diff(json.loads(before), d)
+    assert acl_names(check.acls) == ["example-tree", "example-object"]
 
 
 @pytest.mark.asyncio
