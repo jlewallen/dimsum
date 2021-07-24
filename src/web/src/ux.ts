9ad81b87dd08be09b0f6a20150834980330828c3
Jacob Lewallen <jlewallen@gmail.com>
2021-07-24 12:14:37 -0700

    chore: This is too much.

--- src/web/src/ux.ts
+++ src/web/src/ux.ts
@@ -2,7 +2,7 @@ export function ignoringKey(ev: Event) {
     if (!ev) throw new Error();
     const focused = window.document.activeElement;
     if (focused) {
-        console.log("ignoringKey", focused.localName);
+        // console.log("ignoringKey", focused.localName);
         if (focused.localName == "textarea" || focused.localName == "input") {
             return true;
         }
