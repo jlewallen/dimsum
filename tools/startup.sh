d428c9c907e535674c9a3fb92d5ee25e42e56b19
Jacob Lewallen <jlewallen@gmail.com>
2021-07-30 19:39:41 -0700

    fix: Missed rename.

--- tools/startup.sh
+++ tools/startup.sh
@@ -5,5 +5,5 @@ PATH=/app/venv/bin:$PATH
 nginx -c /app/nginx.conf &
 
 pushd /app
-python cli.py --no-colors server --unix-socket /tmp/uvicorn.sock "$@"
+python ds.py --no-colors server --unix-socket /tmp/uvicorn.sock "$@"
 popd
